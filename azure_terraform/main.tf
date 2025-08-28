fd55409f73d25aa02e7711c4a18bdeb17d0352b8
dprashin <dprashin@gmail.com>
2025-08-28 14:14:23 +0200

    Add network and subnet resource group

--- azure_terraform/main.tf
+++ azure_terraform/main.tf
@@ -20,3 +20,21 @@ resource "azurerm_resource_group" "mtc-rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
