75907dca23d53faa30e8f3c3be1dc57c7a5fa3b5
dprashin <dprashin@gmail.com>
2025-08-28 10:07:24 +0200

    Create resource group in Azure

--- azure_terraform/main.tf
+++ azure_terraform/main.tf
@@ -10,4 +10,13 @@ terraform {
 # Configure the Azure provider
 provider "azurerm" {
   features {}
+  subscription_id = "XXXXXXXXXXXXXXXXX"
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+  }
 }
